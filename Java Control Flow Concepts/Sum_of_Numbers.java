960921b9929d87e9de929ce1393cf337ef039ec9
Yashkumar1160 <yk4442144@gmail.com>
2025-06-13 10:23:10 +0530

    [Yash Kumar] add. Sum of numbers until input is 0. added.

--- Java Control Flow Concepts/Sum_of_Numbers.java
+++ Java Control Flow Concepts/Sum_of_Numbers.java
@@ -2,13 +2,13 @@ import java.util.*;
 public class Sum_of_Numbers{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		double total=0.0;
+		double total_sum=0.0;
 		double input=sc.nextInt();
 		while(input!=0){
-			total=total+input;
+			total_sum=total_sum+input;
 			input=sc.nextInt();		
 		}
-		System.out.println(total);
+		System.out.println(total_sum);
 		
 	}
-}~
+}
