27204431e337d029873b0173e04cd007cb5441f9
Yashkumar1160 <yk4442144@gmail.com>
2025-06-13 10:13:42 +0530

    [Yash Kumat] add. Sum of numbers until input is 0 or negative. added

--- Java Control Flow Concepts/Sum_until_zero_or_negative.java
+++ Java Control Flow Concepts/Sum_until_zero_or_negative.java
@@ -2,14 +2,14 @@ import java.util.*;
 public class Sum_until_zero_or_negative{
 	public static void main(String args[]){
 		Scanner sc=new Scanner(System.in);
-		double total=0.0;
+		double totalSum=0.0;
 		double input=sc.nextInt();
 		while(true){
 			if(input==0||input<0)break;
-			total=total+input;
+			totalSum=totalSum+input;
 			input=sc.nextInt();		
 		}
-		System.out.println(total);
+		System.out.println(totalSum);
 		
 	}
 }
