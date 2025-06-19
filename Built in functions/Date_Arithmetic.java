31a41b823481af59dda1f948519f4f2942783336
Yashkumar1160 <yk4442144@gmail.com>
2025-06-19 11:33:13 +0530

    [Yash Kumar] add. Date Arithmetic operation. added

--- Built in functions/Date_Arithmetic.java
+++ Built in functions/Date_Arithmetic.java
@@ -3,9 +3,13 @@ import java.util.*;
 
 public class Date_Arithmetic {
     public static void main(String args[]) {
-        int year=2023;
-        int month=6;
-        int dd=19;
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter year");
+        int year=sc.nextInt();
+        System.out.println("Enter month");
+        int month=sc.nextInt();
+        System.out.println("Enter dd");
+        int dd=sc.nextInt();
         LocalDate date=LocalDate.of(year,month,dd);
         LocalDate newDate=date.plusDays(7).plusMonths(1).plusYears(2).minusWeeks(3);
         System.out.println("Original date "+date);
