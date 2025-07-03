7fd81c1652eb2e453361e7b46e3210b57ebbe22f
Yashkumar1160 <yk4442144@gmail.com>
2025-07-03 22:04:17 +0530

    [Yash Kumar] add. Implement Book using oops. added

---
+++ Constructors/Level-1/BookClient.java
@@ -0,0 +1,14 @@
+
+public class BookClient {
+	public static void main(String[] args) {
+		Book b = new Book();
+		System.out.println(b.author);
+		System.out.println(b.title);
+		System.out.println(b.price);
+		
+		Book b1 = new Book("title","author",1000);
+		System.out.println(b1.author);
+		System.out.println(b1.title);
+		System.out.println(b1.price);
+	}
+}
