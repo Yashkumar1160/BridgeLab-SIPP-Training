349edde0d06bf1caad956534d0066a3c4a33145d
Yashkumar1160 <yk4442144@gmail.com>
2025-07-03 22:08:02 +0530

    [Yash Kumar] add. Implement Circle client using oops. added

---
+++ Constructors/Level-1/CircleClient.java
@@ -0,0 +1,10 @@
+
+public class CircleClient {
+	public static void main(String[] args) {
+		Circle cr = new Circle();
+		System.out.println(cr.radius);
+		
+		Circle cr1 = new Circle(10);
+		System.out.println(cr1.radius);
+	}
+}
