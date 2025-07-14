6386b7ec7c22c89ef519ecc9c7e06fc6fc17b012
Yashkumar1160 <yk4442144@gmail.com>
2025-07-14 22:14:10 +0530

    [Yash Kumar] add. Convert byte stream to character stream. added

---
+++ Linear_Binary_Search/ReadBytes.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+public class ReadBytes {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt"), "UTF-8"));
+        String line;
+        while ((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
+        br.close();
+    }
+}
