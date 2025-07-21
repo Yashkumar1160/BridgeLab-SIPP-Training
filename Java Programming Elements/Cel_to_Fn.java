9d16bf7a7db05e340e8945c7eaa3d3a7d20510d3
Yashkumar1160 <yk4442144@gmail.com>
2025-07-21 10:44:26 +0530

    Created

--- Java Programming Elements/Cel_to_Fn.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class Cel_to_Fn{
-	public static void main(String args[]){
-		Scanner sc=new Scanner(System.in);
-		float cel=sc.nextFloat();
-		float Fn=(cel*9/5)+32;
-		System.out.print("The "+cel+" celsius is "+Fn+"fahrenheit");
-	}
-}
--- Level-1/Pen_Division.java
+++
@@ -1,9 +0,0 @@
-public class Pen_Division{
-	public static void main(String args[]){
-		int numberOfpens=14;
-		int numberOfstudents=3;
-		int pen_remains=numberOfpens%numberOfstudents;
-		int pen_per_student=numberOfpens/numberOfstudents;
-		System.out.print("The Pen Per Student is "+pen_per_student+" and the remaining pen not distributed is "+pen_remains);
-	}
-}
