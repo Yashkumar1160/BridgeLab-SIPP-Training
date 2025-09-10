9f05b490d8daf6cf994c4b8869199187f6400902
Yashkumar1160 <yk4442144@gmail.com>
2025-09-10 09:04:23 +0530

    [Yash Kumar] add. Updated.

--- Functional_Interfaces/BackgroundJobExecution.java
+++
@@ -1,6 +0,0 @@
-public class BackgroundJobExecution {
-    public static void main(String[] args) {
-        Runnable job = () -> System.out.println("Job executed in background");
-        new Thread(job).start();
-    }
-}
